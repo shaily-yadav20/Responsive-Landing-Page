65bf44ed403702f276aca8109a1d1b3d34403797
SHAILY YADAV <shailyy32@gmail.com>
2025-07-20 10:29:03 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript to detect scroll and change navbar color
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
